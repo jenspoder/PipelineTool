04e9b10e1819005ba3212470192de349c712345b
jenspoder <jens.poder@gmail.com>
2012-06-03 16:20:30 +0200

    navbar version 0.8 - index 0.8

--- personlig.php
+++ personlig.php
@@ -1,10 +1,8 @@
-
-
 <?
 
 /** 
 
-Modellen for denne side er f¿lgende:
+Modellen for denne side er følgende:
 
 Standard
 - Inkluder standardvariabler
@@ -18,9 +16,61 @@ Hent data fra database
 - henter tenders where pipeline-ejer er lig det der er valgt gemmer i array - her efter LOOP der
 	- henter nyeste tender-revision
 	- henter data fra kunde-id'er
-- Render tabellen
-
+- Render tabellen (det interessante bliver hvordan jeg får pipeline til at være specfikt på den relevante afdeling - løses i pulldown
 
 **/
 
+$pipelineejer = 'PJ';
+
 ?>
+
+<html>
+	
+	<head>
+		<meta charset="ISO-8859-1">
+		<title>Personlig pipeline for <? echo "$pipelineejer"; ?></title>			
+	</head>
+	
+	
+	<body>
+		
+		<!-- Her kommer navigation til at være -->
+
+		<div id="navigation">
+			<p>Here be navigation</p>
+			<hr>
+		</div>
+
+		<div id="table-area">
+			<h1>JP's pipeline</h1>
+			
+			<table>
+				<tr>
+					<th>Kunde</th>
+					<th>Projekt</th>
+					<th>Værdi</th>
+					<th>Analyse</th>
+					<th>Sandsynlighed</th>
+					<th>Forventet analyse</th>
+					<th>Timing</th>
+					<th>Sidste kontakt</th>
+					<th>Solgt / Tabt</th>				
+				</tr>
+				<tr>
+					<td>3F</td>
+					<td>Mobil spørgeskema</td>
+					<td>100000</td>					
+					<td>50000</td>
+					<td>55%</td>
+					<td>27000</td>
+					<td>Q3</td>
+					<td>2012-06-03</td>
+					<td>X / V</td>
+				</tr>
+			</table>
+		</div>		
+	 
+	 <p><a href="#">Tilføj nyt projekt</a></p>
+		
+	</body>
+</html>
